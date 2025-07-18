fffa433b474621e30448c3ba37560ac4add90974
Otavio Santana <otaviopolianasantana@gmail.com>
2025-07-18 19:23:39 +0100

    chore: initial test structure
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

---
+++ src/test/java/org/soujava/demos/mongodb/document/AppTest.java
@@ -0,0 +1,15 @@
+package org.soujava.demos.mongodb.document;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AppTest {
+
+
+    @Test
+    void shouldTest() {
+
+    }
+
+}
