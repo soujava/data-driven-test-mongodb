1aad15097132e3824db60fcbcf2897e96314a585
Otavio Santana <otaviopolianasantana@gmail.com>
2025-07-19 22:54:19 +0100

    test: update mangae supplier
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- src/test/java/org/soujava/demos/mongodb/document/ManagerSupplier.java
+++ src/test/java/org/soujava/demos/mongodb/document/ManagerSupplier.java
@@ -5,8 +5,6 @@ import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.enterprise.inject.Alternative;
 import jakarta.enterprise.inject.Produces;
 import jakarta.interceptor.Interceptor;
-import org.eclipse.jnosql.communication.Settings;
-import org.eclipse.jnosql.databases.mongodb.communication.MongoDBDocumentConfiguration;
 import org.eclipse.jnosql.databases.mongodb.communication.MongoDBDocumentManager;
 
 import java.util.function.Supplier;
@@ -18,9 +16,6 @@ public class ManagerSupplier implements Supplier<MongoDBDocumentManager> {
 
     @Produces
     public MongoDBDocumentManager get() {
-        Settings settings = Settings.builder().put("credential", "value").build();
-        MongoDBDocumentConfiguration configuration = new MongoDBDocumentConfiguration();
-        var factory = configuration.apply(settings);
-        return factory.apply("database");
+        return DocumentDatabase.INSTANCE.get("database");
     }
 }
