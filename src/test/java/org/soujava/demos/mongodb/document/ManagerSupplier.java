732e0b600607125326b0027343e758ef3e261660
Otavio Santana <otaviopolianasantana@gmail.com>
2025-07-19 23:06:52 +0100

    test: update manager supplier
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- src/test/java/org/soujava/demos/mongodb/document/ManagerSupplier.java
+++ src/test/java/org/soujava/demos/mongodb/document/ManagerSupplier.java
@@ -3,19 +3,24 @@ package org.soujava.demos.mongodb.document;
 import jakarta.annotation.Priority;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.enterprise.inject.Alternative;
+import jakarta.enterprise.inject.Default;
 import jakarta.enterprise.inject.Produces;
 import jakarta.interceptor.Interceptor;
-import org.eclipse.jnosql.databases.mongodb.communication.MongoDBDocumentManager;
+import org.eclipse.jnosql.communication.semistructured.DatabaseManager;
+import org.eclipse.jnosql.mapping.Database;
+import org.eclipse.jnosql.mapping.DatabaseType;
 
 import java.util.function.Supplier;
 
 @ApplicationScoped
 @Alternative
 @Priority(Interceptor.Priority.APPLICATION)
-public class ManagerSupplier implements Supplier<MongoDBDocumentManager> {
+public class ManagerSupplier implements Supplier<DatabaseManager> {
 
     @Produces
-    public MongoDBDocumentManager get() {
+    @Database(DatabaseType.DOCUMENT)
+    @Default
+    public DatabaseManager get() {
         return DocumentDatabase.INSTANCE.get("database");
     }
 }
