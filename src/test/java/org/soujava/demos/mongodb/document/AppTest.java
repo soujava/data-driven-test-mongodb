8f1fcd981d1d4f07415ae55a7dd3cf6b83e79c76
Otavio Santana <otaviopolianasantana@gmail.com>
2025-07-19 22:45:44 +0100

    test: update app test
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- src/test/java/org/soujava/demos/mongodb/document/AppTest.java
+++ src/test/java/org/soujava/demos/mongodb/document/AppTest.java
@@ -1,11 +1,31 @@
 package org.soujava.demos.mongodb.document;
 
+import io.smallrye.config.Converters;
+import org.eclipse.jnosql.databases.mongodb.mapping.MongoDBTemplate;
+import org.eclipse.jnosql.mapping.Database;
+import org.eclipse.jnosql.mapping.document.DocumentTemplate;
+import org.eclipse.jnosql.mapping.document.spi.DocumentExtension;
+import org.eclipse.jnosql.mapping.reflection.spi.ReflectionEntityMetadataExtension;
+import org.eclipse.jnosql.mapping.semistructured.EntityConverter;
+import org.jboss.weld.junit5.auto.AddExtensions;
+import org.jboss.weld.junit5.auto.AddPackages;
+import org.jboss.weld.junit5.auto.EnableAutoWeld;
+import org.jboss.weld.util.reflection.Reflections;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+
+@EnableAutoWeld
+@AddPackages(value = {Database.class, EntityConverter.class, DocumentTemplate.class, MongoDBTemplate.class})
+@AddPackages(App.class)
+@AddPackages(MongoDBTemplate.class)
+@AddPackages(Reflections.class)
+@AddPackages(Converters.class)
+@AddExtensions({ReflectionEntityMetadataExtension.class, DocumentExtension.class})
 class AppTest {
 
+    private DocumentTemplate template;
 
     @Test
     void shouldTest() {
