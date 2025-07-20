83f7bb9720f81e9b61b5733953b7aaeac863cde3
Otavio Santana <otaviopolianasantana@gmail.com>
2025-07-20 18:36:04 +0100

    feat: include room entity equals and toString
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- src/main/java/org/soujava/demos/mongodb/document/Room.java
+++ src/main/java/org/soujava/demos/mongodb/document/Room.java
@@ -4,6 +4,8 @@ import jakarta.nosql.Column;
 import jakarta.nosql.Entity;
 import jakarta.nosql.Id;
 
+import java.util.Objects;
+
 @Entity
 public class Room {
 
@@ -28,4 +30,59 @@ public class Room {
     @Column
     private boolean underMaintenance;
 
+    public String getId() {
+        return id;
+    }
+
+    public int getRoomNumber() {
+        return roomNumber;
+    }
+
+    public RoomType getType() {
+        return type;
+    }
+
+    public RoomStatus getStatus() {
+        return status;
+    }
+
+    public CleanStatus getCleanStatus() {
+        return cleanStatus;
+    }
+
+    public boolean isSmokingAllowed() {
+        return smokingAllowed;
+    }
+
+    public boolean isUnderMaintenance() {
+        return underMaintenance;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Room room = (Room) o;
+        return Objects.equals(id, room.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Room{" +
+                "id='" + id + '\'' +
+                ", roomNumber=" + roomNumber +
+                ", type=" + type +
+                ", status=" + status +
+                ", cleanStatus=" + cleanStatus +
+                ", smokingAllowed=" + smokingAllowed +
+                ", underMaintenance=" + underMaintenance +
+                '}';
+    }
 }
